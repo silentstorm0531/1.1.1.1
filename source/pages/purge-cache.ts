adba28fb71ea826b95f40f8ab64a7c566b5e3595
vasco <vasco@cloudflare.com>
2018-07-16 17:56:28 -0700

    RES-294: First functional version of the UI

--- source/pages/purge-cache.ts
+++ source/pages/purge-cache.ts
@@ -1,7 +1,43 @@
 import '../styles/base.styl'
 
-async function init () {
+const SUCCESS_MSG = 'Purge request queued. Please wait a few minutes and verify the request was successful.'
 
+async function init () {
+  const form = <HTMLFormElement>document.getElementById('purge-cache-form')!
+  const submitButton = <HTMLElement>document.getElementById('submit-button')!
+  const info = <HTMLElement>document.getElementById('info-message')!
+
+  form.onsubmit = function(evt) {
+    evt.preventDefault()
+
+    const params = new FormData(form)
+    const qs = ['domain', 'type'].map(p => `${p}=${params.get(p)}`).join('&')
+
+    setLoading(true)
+    fetch(`https://resolver-cache-purger.cfdata.org?${qs}`, {
+      method: 'POST'
+    })
+      .then(res => {
+        setMessage(res.ok ? SUCCESS_MSG : `(${res.status}: ${res.statusText}) ${res.body}`, !res.ok)
+      })
+      .catch(err => setMessage(err, true))
+      .then(() => setLoading(false))
+  }
+
+  function setLoading (loading: boolean) {
+    submitButton.setAttribute('value', loading ? 'Sending...' : 'Purge Cache')
+    if (loading) {
+      submitButton.setAttribute('disabled', '')
+    } else {
+      submitButton.removeAttribute('disabled')
+    }
+  }
+
+  function setMessage (message: string, error: boolean) {
+    info.textContent = message
+    info.classList.add(error ? 'error' : 'success')
+    info.classList.remove(!error ? 'error' : 'success')
+  }
 }
 
 if (document.readyState === 'loading') {
