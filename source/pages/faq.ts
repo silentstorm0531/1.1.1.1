89ddb29559eee02f49ae5b2166b65b361e0c4eaa
Patrick Lu <plu@cloudflare.com>
2018-10-27 16:18:48 -0700

    Add faq page

---
+++ source/pages/faq.ts
@@ -0,0 +1,7 @@
+import "../styles/base.styl";
+
+import SweetScroll from "sweet-scroll";
+
+function init() {
+  const scroller = new SweetScroll(); // Initialized for its DOM side effects.
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,112 +1,113 @@
-import { port } from './source/utilities/routes'
+import { port } from "./source/utilities/routes";
 // import * as defaultsDeep from 'lodash.defaultsdeep'
-const defaultsDeep = require('lodash.defaultsdeep')
+const defaultsDeep = require("lodash.defaultsdeep");
 import webpack, {
   DefinePlugin,
   NamedModulesPlugin,
   HotModuleReplacementPlugin,
   NoEmitOnErrorsPlugin,
   SourceMapDevToolPlugin
-} from 'webpack'
-import * as HtmlWebpackPlugin from 'html-webpack-plugin'
-import * as CleanWebpackPlugin from 'clean-webpack-plugin'
-import * as CopyWebpackPlugin from 'copy-webpack-plugin'
-import * as OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
-import { format as formatURL } from 'url'
-import localeDefinitions from './source/utilities/i18n/lang'
+} from "webpack";
+import * as HtmlWebpackPlugin from "html-webpack-plugin";
+import * as CleanWebpackPlugin from "clean-webpack-plugin";
+import * as CopyWebpackPlugin from "copy-webpack-plugin";
+import * as OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
+import { format as formatURL } from "url";
+import localeDefinitions from "./source/utilities/i18n/lang";
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
-const environment = (process.env.NODE_ENV || 'development') as webpack.Configuration['mode']
-const path = require('path')
-const joinP = path.join.bind(null, __dirname)
-const fs = require('fs')
+const environment = (process.env.NODE_ENV ||
+  "development") as webpack.Configuration["mode"];
+const path = require("path");
+const joinP = path.join.bind(null, __dirname);
+const fs = require("fs");
 
-const exclude = /node_modules/
+const exclude = /node_modules/;
 
 interface CustomConfiguration extends webpack.Configuration {
   entry: {
-    [index: string]: string[]
-  }
-  plugins: webpack.Plugin[]
+    [index: string]: string[];
+  };
+  plugins: webpack.Plugin[];
 }
 
 const $: CustomConfiguration = {
   mode: environment,
   entry: {
-    site: ['./source/pages/index.ts'],
-    help: ['./source/pages/help.ts'],
-    'purgeCache': ['./source/pages/purge-cache.ts']
+    site: ["./source/pages/index.ts"],
+    help: ["./source/pages/help.ts"],
+    purgeCache: ["./source/pages/purge-cache.ts"],
+    faq: ["./source/pages/faq.ts"]
   },
   plugins: []
-}
+};
 
 $.resolve = {
-  extensions: ['.js', '.json', '.ts', '.tsx'],
-  modules: [joinP('source'), joinP('node_modules')]
-}
+  extensions: [".js", ".json", ".ts", ".tsx"],
+  modules: [joinP("source"), joinP("node_modules")]
+};
 
 $.output = {
-  publicPath: '/',
+  publicPath: "/",
   // publicPath: '',
-  path: joinP('build'),
-  filename: '[name]-[hash].js',
-  library: '[name]-[hash]',
-  libraryTarget: 'umd'
-}
+  path: joinP("build"),
+  filename: "[name]-[hash].js",
+  library: "[name]-[hash]",
+  libraryTarget: "umd"
+};
 
 export interface BundleDefinition {
-  [index: string]: string | object,
+  [index: string]: string | object;
 }
 
 const bundleDefinitions: BundleDefinition = {
-  'process.env': {
+  "process.env": {
     NODE_ENV: JSON.stringify(environment)
   }
-}
+};
 
 $.plugins = [
   new DefinePlugin(bundleDefinitions),
   new ForkTsCheckerWebpackPlugin({
-    tsconfig: joinP('source/tsconfig.json')
+    tsconfig: joinP("source/tsconfig.json")
   }),
-  new CopyWebpackPlugin([{
-    to: 'media',
-    from: joinP('source/media')
-  }])
-]
+  new CopyWebpackPlugin([
+    {
+      to: "media",
+      from: joinP("source/media")
+    }
+  ])
+];
 
-if (environment === 'development') {
-  $.devtool = 'cheap-module-source-map'
+if (environment === "development") {
+  $.devtool = "cheap-module-source-map";
 
-  $.plugins.push(
-    new NamedModulesPlugin(),
-    new NoEmitOnErrorsPlugin()
-  )
+  $.plugins.push(new NamedModulesPlugin(), new NoEmitOnErrorsPlugin());
 }
 
 let styleLoader = {
-  loader: 'style-loader',
+  loader: "style-loader",
   options: {
     sourceMap: true
   }
-}
+};
 
-if (environment === 'production') {
-  styleLoader = MiniCssExtractPlugin.loader
+if (environment === "production") {
+  styleLoader = MiniCssExtractPlugin.loader;
 
-  $.plugins.unshift(new CleanWebpackPlugin($.output.path!))
+  $.plugins.unshift(new CleanWebpackPlugin($.output.path!));
 
   $.plugins.push(
     new SourceMapDevToolPlugin({
-      filename: '[name]-[hash].map'
+      filename: "[name]-[hash].map"
     }),
     new MiniCssExtractPlugin({
-      filename: '[name]-[hash].css',
-      chunkFilename: '[id].css',
+      filename: "[name]-[hash].css",
+      chunkFilename: "[id].css"
     }),
     new UglifyJsPlugin({
       sourceMap: true,
@@ -119,12 +120,12 @@ if (environment === 'production') {
       }
     }),
     new OptimizeCSSAssetsPlugin()
-  )
+  );
 }
 
-let htmlMinify: boolean | { [index: string]: boolean } = false
+let htmlMinify: boolean | { [index: string]: boolean } = false;
 
-if (environment !== 'development') {
+if (environment !== "development") {
   htmlMinify = {
     html5: true,
     minifyJS: true,
@@ -132,71 +133,74 @@ if (environment !== 'development') {
     collapseWhitespace: true,
     preserveLineBreaks: false,
     removeComments: false
-  }
+  };
 }
 
 const locales = [
   {
-    path: '',
-    code: 'en-US',
-    label: 'English'
+    path: "",
+    code: "en-US",
+    label: "English"
   },
   {
-    path: 'es-ES/',
-    code: 'es-ES',
-    label: 'Español'
+    path: "es-ES/",
+    code: "es-ES",
+    label: "Español"
   },
   {
-    path: 'fr-FR/',
-    code: 'fr-FR',
-    label: 'Français'
+    path: "fr-FR/",
+    code: "fr-FR",
+    label: "Français"
   },
   {
-    path: 'de-DE/',
-    code: 'de-DE',
-    label: 'Deutsch'
+    path: "de-DE/",
+    code: "de-DE",
+    label: "Deutsch"
   },
   {
-    path: 'pt-BR/',
-    code: 'pt-BR',
-    label: 'Português'
+    path: "pt-BR/",
+    code: "pt-BR",
+    label: "Português"
   },
   {
-    path: 'nl-NL/',
-    code: 'nl-NL',
-    label: 'Nederlands'
+    path: "nl-NL/",
+    code: "nl-NL",
+    label: "Nederlands"
   },
   {
-    path: 'tr-TR/',
-    code: 'tr-TR',
-    label: 'Türkçe'
+    path: "tr-TR/",
+    code: "tr-TR",
+    label: "Türkçe"
   },
   {
-    path: 'zh-Hans/',
-    code: 'zh-Hans',
-    label: '简体中文'
+    path: "zh-Hans/",
+    code: "zh-Hans",
+    label: "简体中文"
   },
   {
-    path: 'zh-Hant/',
-    code: 'zh-Hant',
-    label: '繁體中文'
+    path: "zh-Hant/",
+    code: "zh-Hant",
+    label: "繁體中文"
   },
   {
-    path: 'ja-JP/',
-    code: 'ja-JP',
-    label: '日本語'
+    path: "ja-JP/",
+    code: "ja-JP",
+    label: "日本語"
   },
   {
-    path: 'ko-KR/',
-    code: 'ko-KR',
-    label: '한국어'
+    path: "ko-KR/",
+    code: "ko-KR",
+    label: "한국어"
   }
-]
+];
 
 // HACK: For many many reasons, Pug. html-loader, and html-webpack-plugin are incompatible.
 
-const pugLoaders = locales.map((locale) => {
-  const mergedDefinitions = defaultsDeep(localeDefinitions[locale.code], localeDefinitions['en-US'])
+const pugLoaders = locales.map(locale => {
+  const mergedDefinitions = defaultsDeep(
+    localeDefinitions[locale.code],
+    localeDefinitions["en-US"]
+  );
 
   return {
     test: /\.pug$/,
@@ -204,14 +208,14 @@ const pugLoaders = locales.map((locale) => {
     include: new RegExp(`${locale.code}.*\/index\.pug$`),
     use: [
       {
-        loader: 'html-loader',
+        loader: "html-loader",
         options: {
           minimize: htmlMinify,
-          attrs: ['img:src', 'video:src']
+          attrs: ["img:src", "video:src"]
         }
       },
       {
-        loader: 'pug-html-loader',
+        loader: "pug-html-loader",
         options: {
           pretty: false,
           data: {
@@ -228,40 +232,59 @@ const pugLoaders = locales.map((locale) => {
         }
       }
     ]
-  }
-})
+  };
+});
 
-$.plugins.push(...locales.map((locale) => {
-  return new HtmlWebpackPlugin({
-    favicon: 'source/media/favicon.png',
-    template: joinP(`source/pages/${locale.code}/index.pug`),
-    filename: `${locale.path}index.html`,
-    chunks: ['site']
+$.plugins.push(
+  ...locales.map(locale => {
+    return new HtmlWebpackPlugin({
+      favicon: "source/media/favicon.png",
+      template: joinP(`source/pages/${locale.code}/index.pug`),
+      filename: `${locale.path}index.html`,
+      chunks: ["site"]
+    });
   })
-}))
+);
 
-$.plugins.push(...locales.map((locale) => {
-  return new HtmlWebpackPlugin({
-    favicon: 'source/media/favicon.png',
-    template: joinP(`source/pages/${locale.code}/help/index.pug`),
-    filename: `${locale.path}help/index.html`,
-    chunks: ['help']
+$.plugins.push(
+  ...locales.map(locale => {
+    return new HtmlWebpackPlugin({
+      favicon: "source/media/favicon.png",
+      template: joinP(`source/pages/${locale.code}/faq/index.pug`),
+      filename: `${locale.path}faq/index.html`,
+      chunks: ["faq"]
+    });
   })
-}))
+);
 
-$.plugins.push(...locales.map((locale) => {
-  return new HtmlWebpackPlugin({
-    favicon: 'source/media/favicon.png',
-    template: joinP(`source/pages/${locale.code}/purge-cache/index.pug`),
-    filename: `${locale.path}purge-cache/index.html`,
-    chunks: ['purgeCache']
+$.plugins.push(
+  ...locales.map(locale => {
+    return new HtmlWebpackPlugin({
+      favicon: "source/media/favicon.png",
+      template: joinP(`source/pages/${locale.code}/help/index.pug`),
+      filename: `${locale.path}help/index.html`,
+      chunks: ["help"]
+    });
   })
-}))
+);
 
-$.plugins.push(new HtmlWebpackPlugin({
-  template: 'source/pages/404.html',
-  filename: '404.html'
-}))
+$.plugins.push(
+  ...locales.map(locale => {
+    return new HtmlWebpackPlugin({
+      favicon: "source/media/favicon.png",
+      template: joinP(`source/pages/${locale.code}/purge-cache/index.pug`),
+      filename: `${locale.path}purge-cache/index.html`,
+      chunks: ["purgeCache"]
+    });
+  })
+);
+
+$.plugins.push(
+  new HtmlWebpackPlugin({
+    template: "source/pages/404.html",
+    filename: "404.html"
+  })
+);
 
 $.module = {
   rules: [
@@ -270,10 +293,10 @@ $.module = {
       exclude,
       use: [
         {
-          loader: 'ts-loader',
+          loader: "ts-loader",
           options: {
             transpileOnly: true, // Type checking done via plugin.
-            configFile: joinP('source/tsconfig.json')
+            configFile: joinP("source/tsconfig.json")
           }
         }
       ]
@@ -283,7 +306,7 @@ $.module = {
       test: /\.(png|jpg|gif|mp4)$/,
       use: [
         {
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
             // name: '[path][name].[ext]'
           }
@@ -292,7 +315,7 @@ $.module = {
     },
     {
       test: /\.svg$/,
-      loader: 'svg-inline-loader',
+      loader: "svg-inline-loader",
       exclude
     },
     {
@@ -301,13 +324,13 @@ $.module = {
       use: [
         styleLoader,
         {
-          loader: 'css-loader',
+          loader: "css-loader",
           options: {
             sourceMap: true
           }
         },
         {
-          loader: 'stylus-loader',
+          loader: "stylus-loader",
           options: {
             sourceMap: true
           }
@@ -315,6 +338,6 @@ $.module = {
       ]
     }
   ]
-}
+};
 
-export default $
+export default $;
