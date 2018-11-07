85221ad9a7d97beff5975aa83fa8190aeea9b8e2
Patrick Lu <plu@cloudflare.com>
2018-11-06 17:48:45 -0800

    Implement language picker on faq page

--- source/utilities/i18n/lang/index.ts
+++ source/utilities/i18n/lang/index.ts
@@ -1,16 +1,15 @@
-const locales: {[index: string]: any} = {}
+const locales: { [index: string]: any } = {};
 
-locales['en-US'] = require('./en-US.json')
-locales['es-ES'] = require('./es-ES.json')
-locales['fr-FR'] = require('./fr-FR.json')
-locales['de-DE'] = require('./de-DE.json')
-locales['nl-NL'] = require('./nl-NL.json')
-locales['zh-Hans'] = require('./zh-Hans.json')
-locales['zh-Hant'] = require('./zh-Hant.json')
-locales['ja-JP'] = require('./ja-JP.json')
-locales['ko-KR'] = require('./ko-KR.json')
-locales['pt-BR'] = require('./pt-BR.json')
-locales['tr-TR'] = require('./tr-TR.json')
+locales["en-US"] = require("./en-US.json");
+locales["es-ES"] = require("./es-ES.json");
+locales["fr-FR"] = require("./fr-FR.json");
+locales["de-DE"] = require("./de-DE.json");
+locales["nl-NL"] = require("./nl-NL.json");
+locales["zh-Hans"] = require("./zh-Hans.json");
+locales["zh-Hant"] = require("./zh-Hant.json");
+locales["ja-JP"] = require("./ja-JP.json");
+locales["ko-KR"] = require("./ko-KR.json");
+locales["pt-BR"] = require("./pt-BR.json");
+locales["tr-TR"] = require("./tr-TR.json");
 
-
-export default locales
+export default locales;
